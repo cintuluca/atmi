3516cf977b0f123539df70270100524224dd65ac
cintuluca <cintuluca@users.noreply.github.com>
2023-06-10 08:15:47 +0000

    deploy: 881a760734568e6c52d0de2b77dab421a7debbfa

--- search/all_6.js
+++ search/all_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['installation_0',['Installation',['../Installation.html',1,'']]],
+  ['installation_0',['Installation',['../installation.html',1,'']]],
   ['installation_2etxt_1',['installation.txt',['../installation_8txt.html',1,'']]],
   ['instrument_2',['instrument',['../classlib_1_1instrumentObs_1_1instrument.html',1,'lib::instrumentObs']]],
   ['instrumentobs_2epy_3',['instrumentObs.py',['../instrumentObs_8py.html',1,'']]]
